9027db288533bcd710b71eb6821cfbc8647eb734
Vanessa Zambrano <v.nza08@icloud.com>
2019-04-08 22:27:16 +0000

    session 1 unit 6

--- js/script.js
+++ js/script.js
@@ -4,8 +4,16 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
-});
+$.ajax({
+        url:"https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
+        method: "GET",
+        success: function(data) {
+        var imgUrl = data.data[0].images.fixed_height_still.url;
+         alert(imgUrl);  
+         
+        }
+    }); 
+    
+});  
 
+// https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC
